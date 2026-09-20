08ae79fa4a75d2a41d1ec0daec2431a63f2c9093
yeasin <yeasinshuvo76@gmail.com>
2026-09-20 21:00:07 +0600

    selection sort implemented with test case

---
+++ selection_srot/index.py
@@ -0,0 +1,9 @@
+def selection_sort(L):
+     n = len(L)
+     for i in range(0, n -1):
+        index_min = i 
+        for j in range(i+1, n): 
+            if L[j] < L[index_min]: 
+                index_min = j 
+        if index_min != i:
+          L[i], L[index_min] = L[index_min], L[i]
